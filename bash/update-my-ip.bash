a6b86aee72ab7a0720b7315205bc595a5fccbc56
koloqial <koloqial@icloud.com>
2023-09-27 21:14:27 +0100

    Moved logic

--- bash/update-my-ip.bash
+++ bash/update-my-ip.bash
@@ -6,18 +6,19 @@ FILE=./.current-ip-address
 
 if test -f "$FILE"; then
     echo "$FILE exists."
-    RECORDED_IP_ADDRESS=`cat $FILE`
-    if [ "$RECORDED_IP_ADDRESS" = "$IPADDRESS" ]; then
-        echo "they're the same!"
-    else
-        echo "they're not the same, will PATCH with $IPADDRESS"
-        curl -X PATCH \
-            -H "Content-Type: application/json" \
-            -H "Authorization: Bearer $DIGITALOCEAN_TOKEN" \
-            -d "{\"type\":\"A\", \"data\":\"$IPADDRESS\"}" \
-            "https://api.digitalocean.com/v2/domains/$DOMAIN/records/$RECORD_ID"
-    fi
 else 
     touch $FILE
     echo $IPADDRESS > $FILE
+fi
+
+RECORDED_IP_ADDRESS=`cat $FILE`
+if [ "$RECORDED_IP_ADDRESS" = "$IPADDRESS" ]; then
+    echo "they're the same!"
+else
+    echo "they're not the same, will PATCH with $IPADDRESS"
+    curl -X PATCH \
+        -H "Content-Type: application/json" \
+        -H "Authorization: Bearer $DIGITALOCEAN_TOKEN" \
+        -d "{\"type\":\"A\", \"data\":\"$IPADDRESS\"}" \
+        "https://api.digitalocean.com/v2/domains/$DOMAIN/records/$RECORD_ID"
 fi
