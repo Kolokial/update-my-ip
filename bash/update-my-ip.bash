0f003f46c4abc5e1ed1a81ea2da04fb88b9d8589
koloqial <koloqial@icloud.com>
2023-09-27 21:42:15 +0100

    moving stuff back

--- bash/update-my-ip.bash
+++ bash/update-my-ip.bash
@@ -6,21 +6,26 @@ FILE=./.current-ip-address
 
 if test -f "$FILE"; then
     echo "$FILE exists."
+    RECORDED_IP_ADDRESS=`cat $FILE`
+    if [ "$RECORDED_IP_ADDRESS" = "$IPADDRESS" ]; then
+        echo "they're the same!"
+    else
+        echo "they're not the same, will PATCH with $IPADDRESS"
+        curl -X PATCH \
+            -H "Content-Type: application/json" \
+            -H "Authorization: Bearer $DIGITALOCEAN_TOKEN" \
+            -d "{\"type\":\"A\", \"data\":\"$IPADDRESS\"}" \
+            "https://api.digitalocean.com/v2/domains/$DOMAIN/records/$RECORD_ID"
+
+        echo $IPADDRESS > $FILE
+    fi
 else 
     touch $FILE
+    curl -X PATCH \
+            -H "Content-Type: application/json" \
+            -H "Authorization: Bearer $DIGITALOCEAN_TOKEN" \
+            -d "{\"type\":\"A\", \"data\":\"$IPADDRESS\"}" \
+            "https://api.digitalocean.com/v2/domains/$DOMAIN/records/$RECORD_ID"
     echo $IPADDRESS > $FILE
 fi
 
-RECORDED_IP_ADDRESS=`cat $FILE`
-if [ "$RECORDED_IP_ADDRESS" = "$IPADDRESS" ]; then
-    echo "they're the same!"
-else
-    echo "they're not the same, will PATCH with $IPADDRESS"
-    curl -X PATCH \
-        -H "Content-Type: application/json" \
-        -H "Authorization: Bearer $DIGITALOCEAN_TOKEN" \
-        -d "{\"type\":\"A\", \"data\":\"$IPADDRESS\"}" \
-        "https://api.digitalocean.com/v2/domains/$DOMAIN/records/$RECORD_ID"
-
-    echo $IPADDRESS > $FILE        
-fi
